252424449c6f6b436280111a8f810fbfe96d6965
atrexus <110138304+atrexus@users.noreply.github.com>
2024-08-14 10:44:05 -0400

    Remove old formatter

--- src/Unluau.CLI/Program.cs
+++ src/Unluau.CLI/Program.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.Extensions.Logging.Console;
-using Microsoft.Extensions.Logging;
-using System.CommandLine;
+﻿using System.CommandLine;
 using System.CommandLine.Parsing;
-using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Unluau.CLI
 {
@@ -16,18 +13,6 @@ namespace Unluau.CLI
             };
 
             return await rootCommand.InvokeAsync(args);
-        }
-    }
-
-    // Custom Console Formatter for UNIX Timestamp
-    public class UnixTimestampConsoleFormatter : ConsoleFormatter
-    {
-        public UnixTimestampConsoleFormatter() : base("unix") { }
-
-        public override void Write<TState>(in LogEntry<TState> logEntry, IExternalScopeProvider? scopeProvider, TextWriter textWriter)
-        {
-            var unixTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-            textWriter.Write($"{unixTimestamp}: {logEntry.Formatter(logEntry.State, logEntry.Exception)}{Environment.NewLine}");
         }
     }
 }
